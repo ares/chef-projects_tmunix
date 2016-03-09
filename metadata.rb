06ff690b3353a16052964f43c4982c7d2b9c00c1
Marek Hulan <mhulan@redhat.com>
2016-03-09 11:06:45 +0100

    Relax projects dependency

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'ar3s.cz@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures projects_tmunix'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends 'projects', '>= 0.3.0'
+depends 'projects'
